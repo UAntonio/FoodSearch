851e3db40aeebec81a3a2fa99818529974d18da7
Uriel <uantonio@csumb.edu>
2020-11-08 17:16:32 -0800

    adding new icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{useEffect, useState} from "react";
 import RecipeGrid from './recipegrid';
+import Recipe from './recipe';
 import './App.css';
 
 
@@ -24,6 +25,7 @@ const App = () =>{
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipesData(data.hits);
+    console.log(data.hits);
   }
 
   const updateSearch = e =>{
@@ -44,10 +46,27 @@ const App = () =>{
         className = "search-button" type="submit">Search</button>
       </form>
 
-      <RecipeGrid
+      {/* <RecipeGrid
       recipesData = {recipesData}
-      />
+      /> */}
+      <div className = "recipes">
+        {recipesData.map(data =>(
+          <Recipe 
+          key= {data.recipe.label}
+          title = {data.recipe.label}
+          calories = {data.recipe.calories}
+          image = {data.recipe.image}
+          ingredients = {data.recipe.ingredients}
+          dietLabels = {data.recipe.dietLabels}
+          totalNutrients ={data.recipe.totalNutrients}
+          totalPercentage = {data.recipe.totalDaily}
+          />
+        ))};
+        </div>
+        
+           Icons made by <a href="https://www.flaticon.com/authors/pause08" title="Pause08">Pause08</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
     </div>
+    
   );
 }
 
